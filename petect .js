f605fa060ffb3021f95fc2f0754465ba4f85a7e5
ashil004 <pramanikashik007@gmail.com>
2023-08-12 17:23:13 +0600

    scend commit

---
+++ petect .js
@@ -0,0 +1,58 @@
+console.log("\t\t\t\t\t\t\t\t-------UPHOHAR LONGHI--------");
+function palkiLonghi(palki){
+    const price_palki=palki*1160;
+    return price_palki;
+}
+  console.log("Palki price:",palkiLonghi(10));
+
+ function talukdrLongi(talukdar){
+    const Price_talukdar=talukdar*2400;
+    return Price_talukdar;
+ }
+  console.log("Talukdar price:",talukdrLongi(10));
+
+ function ashikLongi(ashik){
+    const Price_ashik=ashik*1020;
+    return Price_ashik;
+ }
+ console.log("Ashik price:",ashikLongi(10));
+ function sobarUporLongi(sobar_upor){
+    const Price_sobar_upor=sobar_upor*1020;
+    return Price_sobar_upor;
+ }
+  console.log("Sobar Upor Price:",sobarUporLongi(10));
+ 
+ function sonkhoLongi(sonkho){
+    const Price_sonkho=sonkho*980;
+    return Price_sonkho;
+ }
+ console.log("Sonkho Price:",sonkhoLongi(10));
+ function defenceLongi(defence){
+    const Price_defence=defence*1260;
+    return Price_defence;
+ }
+  console.log("Defence price:",defenceLongi(10));
+ function sunBirdLongi(sunbird){
+    const Price_sunbird=sunbird*1000;
+    return Price_sunbird;
+ }
+ console.log("Sunbird price:",sunBirdLongi(10));
+ 
+// const Total_price1=palkiLonghi();
+// const Total_price3=ashikLongi();
+// const Total_price4=sonkhoLongi();
+// const Total_price5=defenceLongi();
+// const Total_price6=sunBirdLongi();
+// const Total_price7=sobarUporLongi();
+// const Total_price2=talukdrLongi();
+var total=palkiLonghi(10)+ashikLongi(10)+sonkhoLongi(10)+defenceLongi(10)+sunBirdLongi(10)+sobarUporLongi(10)+talukdrLongi(10);
+console.log( "Total price:",total);
+
+
+
+ 
+ 
+ 
+ 
+ 
+
